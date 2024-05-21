4588ee3e77052fa67ea3f312d8400ab7bba775a1
thepineappleandthepie <g22s7874@campus.ru.ac.za>
2024-05-21 08:27:07 +0200

    setting up and making Reading and printing XML fields

---
+++ src/main/java/sdp/prac2/XMLReader.java
@@ -0,0 +1,41 @@
+package main.java.sdp.prac2;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+public class XMLReader {
+    public static void main(String[] args) {
+        try {
+            File inputFile = new File("input.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(inputFile);
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("record");
+
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node nNode = nList.item(temp);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) nNode;
+                    System.out.println("Name: " + eElement.getElementsByTagName("name").item(0).getTextContent());
+                    System.out.println("Postal/Zip: " + eElement.getElementsByTagName("postalZip").item(0).getTextContent());
+                    System.out.println("Region: " + eElement.getElementsByTagName("region").item(0).getTextContent());
+                    System.out.println("Country: " + eElement.getElementsByTagName("country").item(0).getTextContent());
+                    System.out.println("Address: " + eElement.getElementsByTagName("address").item(0).getTextContent());
+                    System.out.println("List: " + eElement.getElementsByTagName("list").item(0).getTextContent());
+                    System.out.println("-------------------------------------");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
